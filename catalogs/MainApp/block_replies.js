76339ccf37ebc534f702224b1eef9bd3aabfe446
Hanyeol Cho <hanyeol.cho@gmail.com>
2020-02-23 18:58:52 +0900

    Release version 1.0

---
+++ catalogs/MainApp/block_replies.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var hiding_replies_style = document.createElement('style');
+
+    hiding_replies_style.innerHTML = `
+        div .android,
+        div .iphone,
+        .btn_cmt,
+        .txt_cmt,
+        .box_cmtrank,
+        .box_viewrelate,
+        .inner_bnr,
+        .item_bnr,
+        .item_ad,
+        .ad_body,
+        .ad_banner,
+        #daumAdWrap,
+        #daumAd,
+        #footerAd {
+            display: none !important;
+        }
+        .info_thumb .txt_cp {
+            visibility: visible;
+        }
+        .info_thumb {
+            visibility: hidden;
+        }
+    `;
+
+    document.head.appendChild(hiding_replies_style);
+})
