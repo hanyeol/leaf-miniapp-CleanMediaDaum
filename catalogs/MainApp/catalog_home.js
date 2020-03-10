be128ee05c2f42a593c618a85a71046ca2e21958
Hanyeol Cho <hanyeol.cho@gmail.com>
2020-03-10 12:26:50 +0900

    Refine the error handler

--- catalogs/MainApp/catalog_home.js
+++ catalogs/MainApp/catalog_home.js
@@ -7,22 +7,41 @@ function on_loaded() {
 }
 
 function on_web_start(data) {
-    view.object("web").action("show")
-    view.object("section.error").action("hide")
+    if (data["is-for-main-frame"] === "yes") {
+        view.object("section.error").action("hide")
+
+        timeout(1, function() {
+            if (!document.value("web.failed")) {
+                view.object("web").action("show")
+
+                view.object("effect.loading").action("hide")
+            }
+        })
+    }
 }
 
 function on_web_failed(data) {
-    view.object("section.error").action("show")
-    
-    controller.action("toast", { 
-        "message":controller.catalog().string("웹페이지를 로드할 수 없습니다.")
-    })
+    if (data["is-for-main-frame"] === "yes") {
+        view.object("section.error").action("show")
 
-    document.value("web.failed", true)
+        view.object("web").action("hide")
+        view.object("effect.loading").action("hide")
+    
+        controller.action("toast", { 
+            "message":controller.catalog().string("웹페이지를 로드할 수 없습니다.")
+        })
+    
+        document.value("web.failed", true)    
+    }
 }
 
 function reload() {
-    view.object("web").action("reload")
+    timeout(0.5, function() {
+        view.object("web").action("reload")
+    })
+
+    view.object("effect.loading").action("show")
+    view.object("effect.loading").action("play")
 
     document.value("web.failed", false)
 }
